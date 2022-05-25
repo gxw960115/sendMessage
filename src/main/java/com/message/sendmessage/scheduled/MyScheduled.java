4bf474d2be898d8c9e54b2e409ca1b26e1378ca2
gxw960115 <gxw960115@outlook.com>
2022-05-25 10:08:02 +0800

    新增填写日志提醒，打卡提醒，喝水提醒

--- src/main/java/com/message/sendmessage/scheduled/MyScheduled.java
+++ src/main/java/com/message/sendmessage/scheduled/MyScheduled.java
@@ -1,15 +1,15 @@
 package com.message.sendmessage.scheduled;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.message.sendmessage.send.SendMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 /**
  * 功能描述：定时任务执行
@@ -21,8 +21,54 @@ import java.util.List;
 public class MyScheduled {
     @Autowired
     private SendMessage sendMessage;
+    @Value("${she.title}")
+    private String title;
+    @Value("${she.message}")
+    private String message;
+    @Value("${she.title1}")
+    private String title1;
+    @Value("${she.message1}")
+    private String message1;
+    @Value("${she.title2}")
+    private String title2;
+    @Value("${she.message2}")
+    private String message2;
+    @Value("${she.title3}")
+    private String title3;
+    @Value("${she.message3}")
+    private String message3;
+    @Value("${she.hour}:${she.minute}:${she.second}")
+    private String begin;
+    @Value("${she.mail}")
+    private String who;
 
-    @Scheduled(cron = "0 */1 * * * ?")
+    @Scheduled(cron = "0 0/30 17,18,19 * * ?")
+    public void startup() throws ParseException {
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        String end = df.format(new Date());
+        Date sd1 = df.parse(begin);
+        Date sd2 = df.parse(end);
+        if (sd1.after(sd2)) {
+            sendMessage.SendMessage(title, message);
+            sendMessage.SendMessage(title1, message1);
+            System.out.println(title + " 已发送！");
+            System.out.println(title1 + " 已发送！");
+        } else {
+            sendMessage.SendMessage(title2, message2);
+            sendMessage.SendMessage(title3, message3);
+            System.out.println(title2 + " 已发送！");
+            System.out.println(title3 + " 已发送！");
+        }
+    }
+
+    //    @Scheduled(cron = "0 0/30 9,10,11,12,13,14,15,16,17,18 * * ?")
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void startup2() {
+        sendMessage.SendMessage("喝水提醒", "天气炎热！莫生气！莫激动！\n又到喝水时间了！多多喝水呀！");
+        System.out.println("发给 " + who + " 的 喝水提醒 已于 " + new Date().toLocaleString() + " 发送");
+    }
+
+    //    @Scheduled(cron = "0 0/1 * * * ?")
     public void startup1() {
 
         JSONObject object = JSONObject.parseObject(SendMessage.getMail());
@@ -34,8 +80,7 @@ public class MyScheduled {
         String s = split[0];
         String[] split1 = s.split(":");
         String message = split1[1].replace("\"", "");
-        sendMessage.SendMessage("世良世良，收到请回答", message);
-
+        sendMessage.sendHtmlMessage("幽默段子", message);
         System.out.print(new Date().toLocaleString() + " 发送消息：" + message);
     }
 }
